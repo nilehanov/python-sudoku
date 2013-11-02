93535ffa0aab614be7b2a1eef51776e45b72f807
Chris Jacobs <zonk1024@gmail.com>
2013-11-02 02:26:05 -0700

    moving into subclass

--- ml_solver.py
+++ ml_solver.py
@@ -13,44 +13,73 @@ from pybrain.datasets import SupervisedDataSet
 from pybrain.supervised.trainers import BackpropTrainer
 
 DATASET_FILE = 'dataset.pickle'
-DATASET_SIZE = 10000
+DATASET_SIZE = 200
 NETWORK_FILE = 'network.pickle'
 SUDOKU_N = 3
 HIDDEN_LAYERS = 1
 EPOCHS = 10000
 
-def format_puzzle(puz, format_type='input'):
-    output = []
-    mg = puz.masked_grid
-    sol = puz.solution
-    temp_puz = mg if format_type=='input' else sol
-    for square in temp_puz:
-        if square not in puz.possibles:
-            output.extend(.1 for _ in puz.possibles)
-        else:
-            output.extend(0. if option != square else 1. for option in sorted(puz.possibles))
-    return output
+class Sudoku(sudoku.Sudoku):
+    def __init__(self, *args, **kwargs):
+        super(Sudoku, self).__init__(*args, **kwargs)
+        # force population of instance vars
+        _ = self.masked_grid
 
-def make_guess(net_output):
-    net_output = list(net_output)
-    output = []
-    n = SUDOKU_N ** 2
-    for i in range(n ** 2):
-        tmp = net_output[i * n:(i + 1) * n]
-        tmp_ans = tmp.index(max(tmp))
-        output.append(tmp_ans)
-    return output
+    @property
+    def input_fmt(self):
+        return self.format_puzzle(self.masked_grid)
 
-def string_puzzle_from_list(puz):
-    output = ''
-    n = SUDOKU_N ** 2
-    for i in range(n):
-        output += ' '.join(str(int(v)) for v in puz[i * n:(i + 1) * n])
-        output += '\n'
-    return output
+    @property
+    def output_fmt(self):
+        return self.format_puzzle(self.solution)
 
-def compare_strings(one, two):
-    return ''.join(a if a==b else '_' for a, b in itertools.izip(one, two))
+    @classmethod
+    def format_puzzle(cls, puz):
+        output = []
+        possibles = range(SUDOKU_N ** 2)
+        for square in puz:
+            if square not in possibles:
+                output.extend(.1 for _ in possibles)
+            else:
+                output.extend(0. if option != square else 1. for option in possibles)
+        return output
+
+    @classmethod
+    def guess_from_net_output(cls, net_output):
+        # de-numpy for slicing
+        net_output = list(net_output)
+        output = []
+        n = SUDOKU_N ** 2
+        for i in range(n ** 2):
+            tmp = net_output[i * n:(i + 1) * n]
+            tmp_ans = tmp.index(max(tmp))
+            output.append(tmp_ans)
+        return output
+
+    @classmethod
+    def guess_to_string(cls, puz):
+        output = ''
+        n = SUDOKU_N ** 2
+        for i in range(n):
+            output += ' '.join(str(int(v)) for v in puz[i * n:(i + 1) * n])
+            output += '\n'
+        return output
+
+    def compare_guess_to_actual(self, guess):
+        return self.compare_two_puzzle_strings(self.guess_to_string(guess), self.actual)
+
+    @classmethod
+    def compare_two_puzzle_strings(cls, one, two):
+        return ''.join(o if o==t else '_' for o, t in itertools.izip(one, two))
+
+    def __str__(self):
+        output = ''
+        n = SUDOKU_N ** 2
+        for i in range(n):
+            output += ' '.join(str(int(v)) for v in self.solution[i * n:(i + 1) * n])
+            output += '\n'
+        return output
+
 
 def save_network(net):
     with open(NETWORK_FILE, 'w') as f:
@@ -61,50 +90,68 @@ def save_dataset(ds):
         pickle.dump(ds, f)
 
 
-if '--fresh' in sys.argv or NETWORK_FILE not in os.listdir('.'):
-    print 'New network'
-    net = buildNetwork(SUDOKU_N ** 6, HIDDEN_LAYERS, SUDOKU_N ** 6, bias=True, hiddenclass=SigmoidLayer)
-else:
-    print 'Loading network'
-    with open(NETWORK_FILE) as f:
-        net = pickle.load(f)
-        net.sorted = False
-        net.sortModules()
+if __name__ == '__main__':
+    # setup network
+    if '--new-net' in sys.argv or NETWORK_FILE not in os.listdir('.'):
+        print 'New network'
+        net = buildNetwork(SUDOKU_N ** 6, HIDDEN_LAYERS, SUDOKU_N ** 6, bias=True, hiddenclass=SigmoidLayer)
+    else:
+        print 'Loading network'
+        with open(NETWORK_FILE) as f:
+            net = pickle.load(f)
+            net.sorted = False
+            net.sortModules()
 
-if DATASET_FILE not in os.listdir('.'):
-    print 'New dataset'
-    ds = SupervisedDataSet(SUDOKU_N ** 6, SUDOKU_N ** 6)
-else:
-    print 'Loading dataset'
-    with open(DATASET_FILE) as f:
-        ds = pickle.load(f)
-trainer = BackpropTrainer(net, ds)
+    # setup dataset
+    if '--new-ds' in sys.argv or DATASET_FILE not in os.listdir('.'):
+        print 'New dataset'
+        ds = SupervisedDataSet(SUDOKU_N ** 6, SUDOKU_N ** 6)
+    else:
+        print 'Loading dataset'
+        with open(DATASET_FILE) as f:
+            ds = pickle.load(f)
 
-while len(ds) < DATASET_SIZE:
-    puzzle = sudoku.Sudoku()
-    time.sleep(.1)
-    f_in  = format_puzzle(puzzle)
-    f_out = format_puzzle(puzzle, format_type='output')
-    ds.addSample(f_in, f_out)
-    ds.addSample(f_out, f_out)
-save_dataset(ds)
+    # setup trainer
+    trainer = BackpropTrainer(net, ds)
 
-print 'Dataset size: {}'.format(len(ds))
+    # ensures dataset is at least this big
+    while len(ds) < DATASET_SIZE:
+        puzzle = Sudoku()
+        ds.addSample(puzzle.input_fmt, puzzle.output_fmt)
+        ds.addSample(puzzle.output_fmt, puzzle.output_fmt)
+    # stash it
+    print 'Dataset size: {}'.format(len(ds))
+    save_dataset(ds)
 
-puzzle = sudoku.Sudoku()
-print 'Starting hits:'
-known = make_guess(format_puzzle(puzzle, format_type='output'))
-known_string = string_puzzle_from_list(known)
-guess = make_guess(net.activate(format_puzzle(puzzle, format_type='output')))
-guess_string = string_puzzle_from_list(guess)
-print compare_strings(known_string, guess_string)
+    # see what it looks like to start
+    print 'Starting hits:'
+    puzzle = Sudoku()
 
-pre = 0
-for i in range(EPOCHS):
-    cur = trainer.train()
-    delta = cur - pre
-    pre = cur
-    print 'Epoch: {:>5}\tError: {:9.7f}\tDelta: {:9.7f}'.format(i + 1, cur, delta)
-    print 'Hits:'
-    print compare_strings(known_string, string_puzzle_from_list(make_guess(net.activate(format_puzzle(puzzle, format_type='output')))))
-    save_network(net)
+    print puzzle
+    print puzzle.output_fmt
+    res = net.activate(puzzle.output_fmt)
+    print Sudoku.guess_to_string(res)
+
+    #known = make_guess(format_puzzle(puzzle, format_type='output'))
+    #known_string = string_puzzle_from_list(known)
+    #guess = make_guess(net.activate(format_puzzle(puzzle, format_type='output')))
+    #guess_string = string_puzzle_from_list(guess)
+    #print compare_strings(known_string, guess_string)
+
+    #pre = 0
+    #pre_time = time.time()
+    #for i in range(EPOCHS):
+    #    puzzle = Sudoku()
+    #    cur = trainer.train()
+    #    delta = cur - pre
+    #    pre = cur
+    #    cur_time = time.time()
+    #    time_delta = cur_time - pre_time
+    #    pre_time = cur_time
+    #    print 'Epoch: {:>5}\tError: {:9.7f}\tDelta: {:9.7f}\tTime delta: {}'.format(i + 1, cur, delta, time_delta)
+    #    puzzle.print_masked()
+    #    print 'Hits:'
+    #    known = make_guess(format_puzzle(puzzle, format_type='output'))
+    #    known_string = string_puzzle_from_list(known)
+    #    print compare_strings(known_string, string_puzzle_from_list(make_guess(net.activate(format_puzzle(puzzle, format_type='output')))))
+    #    save_network(net)
