5d80365523965c7ea0c7e88b615935d3b7e97cef
Chris Jacobs <zonk1024@gmail.com>
2013-11-01 22:02:09 -0700

    another commit

--- ml_solver.py
+++ ml_solver.py
@@ -2,10 +2,13 @@
 
 import os
 import sys
+import time
 import pickle
+import itertools
 import sudokulib.sudoku as sudoku
 from pybrain.tools.shortcuts import buildNetwork
-from pybrain.structure import TanhLayer
+#from pybrain.structure import TanhLayer
+from pybrain.structure import SigmoidLayer
 from pybrain.datasets import SupervisedDataSet
 from pybrain.supervised.trainers import BackpropTrainer
 
@@ -13,18 +16,41 @@ DATASET_FILE = 'dataset.pickle'
 DATASET_SIZE = 10000
 NETWORK_FILE = 'network.pickle'
 SUDOKU_N = 3
-HIDDEN_LAYERS = 5
+HIDDEN_LAYERS = 1
 EPOCHS = 10000
 
 def format_puzzle(puz, format_type='input'):
     output = []
-    temp_puz = puz.masked_grid if format_type=='input' else puz.solution
+    mg = puz.masked_grid
+    sol = puz.solution
+    temp_puz = mg if format_type=='input' else sol
     for square in temp_puz:
         if square not in puz.possibles:
-            output.extend(0 for _ in puz.possibles)
+            output.extend(.1 for _ in puz.possibles)
         else:
-            output.extend(-1 if option != square else 1 for option in sorted(puz.possibles))
+            output.extend(0. if option != square else 1. for option in sorted(puz.possibles))
     return output
+
+def make_guess(net_output):
+    net_output = list(net_output)
+    output = []
+    n = SUDOKU_N ** 2
+    for i in range(n ** 2):
+        tmp = net_output[i * n:(i + 1) * n]
+        tmp_ans = tmp.index(max(tmp))
+        output.append(tmp_ans)
+    return output
+
+def string_puzzle_from_list(puz):
+    output = ''
+    n = SUDOKU_N ** 2
+    for i in range(n):
+        output += ' '.join(str(int(v)) for v in puz[i * n:(i + 1) * n])
+        output += '\n'
+    return output
+
+def compare_strings(one, two):
+    return ''.join(a if a==b else '_' for a, b in itertools.izip(one, two))
 
 def save_network(net):
     with open(NETWORK_FILE, 'w') as f:
@@ -34,28 +60,16 @@ def save_dataset(ds):
     with open(DATASET_FILE, 'w') as f:
         pickle.dump(ds, f)
 
-#puzzle = sudoku.Sudoku()
-#puzzle.print_masked()
-#fp = format_puzzle(puzzle)
-#fp_solved = format_puzzle(puzzle, format_type='output')
-#
-#print
-#print fp
-#print
-#print len(fp)
-#print
-#print fp_solved
-#print
-#print len(fp_solved)
-#ds.addSample(fp, fp_solved)
 
 if '--fresh' in sys.argv or NETWORK_FILE not in os.listdir('.'):
     print 'New network'
-    net = buildNetwork(SUDOKU_N ** 6, HIDDEN_LAYERS, SUDOKU_N ** 6, bias=True, hiddenclass=TanhLayer)
+    net = buildNetwork(SUDOKU_N ** 6, HIDDEN_LAYERS, SUDOKU_N ** 6, bias=True, hiddenclass=SigmoidLayer)
 else:
     print 'Loading network'
     with open(NETWORK_FILE) as f:
         net = pickle.load(f)
+        net.sorted = False
+        net.sortModules()
 
 if DATASET_FILE not in os.listdir('.'):
     print 'New dataset'
@@ -68,17 +82,29 @@ trainer = BackpropTrainer(net, ds)
 
 while len(ds) < DATASET_SIZE:
     puzzle = sudoku.Sudoku()
-    ds.addSample(format_puzzle(puzzle), format_puzzle(puzzle, format_type='output'))
+    time.sleep(.1)
+    f_in  = format_puzzle(puzzle)
+    f_out = format_puzzle(puzzle, format_type='output')
+    ds.addSample(f_in, f_out)
+    ds.addSample(f_out, f_out)
+save_dataset(ds)
 
-print len(ds)
+print 'Dataset size: {}'.format(len(ds))
 
 puzzle = sudoku.Sudoku()
-print format_puzzle(puzzle)[:9]
-print net.activate(format_puzzle(puzzle))[:9]
-print
+print 'Starting hits:'
+known = make_guess(format_puzzle(puzzle, format_type='output'))
+known_string = string_puzzle_from_list(known)
+guess = make_guess(net.activate(format_puzzle(puzzle, format_type='output')))
+guess_string = string_puzzle_from_list(guess)
+print compare_strings(known_string, guess_string)
+
+pre = 0
 for i in range(EPOCHS):
-    print trainer.train()
-    print ' '.join('{:6.3f}'.format(x) for x in net.activate(format_puzzle(puzzle))[:9])
-    print ' '.join('{:6.3f}'.format(float(x)) for x in format_puzzle(puzzle, format_type='output')[:9])
+    cur = trainer.train()
+    delta = cur - pre
+    pre = cur
+    print 'Epoch: {:>5}\tError: {:9.7f}\tDelta: {:9.7f}'.format(i + 1, cur, delta)
+    print 'Hits:'
+    print compare_strings(known_string, string_puzzle_from_list(make_guess(net.activate(format_puzzle(puzzle, format_type='output')))))
     save_network(net)
-    save_dataset(ds)
